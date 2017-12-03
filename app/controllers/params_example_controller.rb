4ea72bc1048a2b7a2ae30ebcd3d35aef1761195c
Brian <riceb53@gmail.com>
2017-12-03 10:38:57 -0600

    make number guessing game

--- app/controllers/params_example_controller.rb
+++ app/controllers/params_example_controller.rb
@@ -6,4 +6,29 @@ class ParamsExampleController < ApplicationController
     p "here is a param thing"
     render json: {message: the_message, message2: other_message}
   end
+
+  def name_game
+    my_name = params["name"].downcase
+    if my_name.starts_with?('a')
+      my_message = "hey your name starts with the first letter of the alphabet"
+    else
+      my_message = "You are not cool"
+    end
+    render json: {name: my_name.upcase, message: my_message}
+  end
+
+  def number_game
+    # answer = rand(100) + 1
+    answer = 36
+    user_input = params["guess"].to_i
+    if user_input == answer
+      my_message = "You win"
+    elsif user_input > answer
+      my_message = "You guessed too high"
+    else
+      my_message = "You guessed too low"
+
+    end
+    render json: {message: my_message}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get "/query_line_parameter_url" => 'params_example#query_line_parameter_method'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/name-game' => 'params_example#name_game'
+  get '/number-game' => 'params_example#number_game'
+  # get '/url' => 'controller#method'
 end
